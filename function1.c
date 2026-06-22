f8bf31b6d237dbd88a0a7bca6678cb238c347c3c
kinggot1004-commits <kinggot1004@dimigo.hs.kr>
2026-06-23 08:14:09 +0900

    Update function1.c

--- function1.c
+++ function1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
 int show_bonguan_3(){
     printf("남학생  교복실 문서   302호 303호 304호   1-1반   미화원    남자        \n");
     printf("탈의실        보관실                             휴게공간  화장실      계단\n");
@@ -37,7 +36,6 @@ int show_bonguan_2(){
     printf("                                                                          \n");
     printf("                                                                     교구관리실2\n");
     printf("\n");
-    printf("                                                                  정보보호관리실습실\n");
     printf("                                                     2-2반\n"); 
     printf("\n");
     printf("                                                     2-5반\n");
@@ -121,42 +119,15 @@ int show_map(int a){
             switch(floor){
                 case 1:
                     printf("-------------------------------------\n");
-                    printf("                시청각실\n");
-                    printf("\n");
-                    printf("                       IT\n");
-                    printf("                    프로젝트실\n");
-                    printf("\n");
-                    printf("   사물인터넷서비스\n");
-                    printf("      기획실습실      화장실\n");
-                    printf("\n");
-                    printf("       계단        엘리베이터       휴머노이드연구소     매점\n");
-                    printf("                                                                매점 테라스\n");
-                    printf("     세미나실                        3학년부 교무실\n");
-                    printf("                 입구\n");
+                    sin_guan_1();
                     printf("-------------------------------------\n");
                 case 2:
                     printf("-------------------------------------\n");
-                    printf("                시청각실\n");
-                    printf("It정보과학          3-6반\n");
-                    printf("영재학급            3-4반\n");
-                    printf("방송콘텐츠제작실습실\n");
-                    printf("멀티미디어실         3-2반\n");
-                    printf("윈도우앱창작터실습실\n");
-                    printf("      화장실         화장실\n");
-                    printf("\n");
-                    printf("       계단        엘리베이터              3-5반\n");
-                    printf("                                                            음악실\n"); 
-                    printf("     연구실                3-1반  3-3반              계단\n");
-                    printf("\n");
+                    sin_guan_2();
                     printf("-------------------------------------\n");
                 case 3:
-                printf("-------------------------------------\n");
-                    printf("도서관\n");
-                    printf("\n");
-                    printf("       계단             엘리베이터    데이터베이스프로그래밍실습실\n");
-                    printf("                                                            음악실\n"); 
-                    printf("                        이사장실   애니메이션컨텐츠  첨단영상            계단\n");
-                    printf("                                      제작실습실      제작실\n");
+                    printf("-------------------------------------\n");
+                    sin_guan_3();
                     printf("-------------------------------------\n");
                 case 4:
                     printf("-------------------------------------\n");
@@ -185,60 +156,28 @@ int show_map(int a){
             switch(floor){
                 case 1:
                     printf("-------------------------------------\n");
-                    printf("	     신발장      계단          입구\n");
-                    printf("독서실\n");
-                    printf("						                    화장실\n");
-                    printf("\n");
-                    printf("	                                       심야자습실\n");
-                    printf("        	             신발장\n");
-                    printf("\n");
-                    printf("        	                       심야자습실\n");
+                    dorm_1();
                     printf("-------------------------------------\n");
                     break;
                 case 2:
                     printf("-------------------------------------\n");
-                    printf("  입구                계단\n");
-                    printf("                                  화장실\n");
-                    printf("  교무실                                      호실\n");
-                    printf("                                  샤워실\n");
-                    printf("\n");  
-                    printf("         샤워실\n");
-                    printf("\n");
-                    printf("        	            호실\n");
+                    dorm_2();
                     printf("-------------------------------------\n");
                     break;
                 case 3:
                     printf("-------------------------------------\n");
-                    printf("               휴게실       계단\n");
-                    printf("                                    화장실\n");
-                    printf("호실						                       호실\n");
-                    printf("         교무실\n");  
-                    printf("	                                 샤워실\n");
-                    printf("        	    샤워실\n");
-                    printf("\n");
-                    printf("        	             호실\n");
+                    dorm_3();
                     printf("-------------------------------------\n");
                     break;
                 case 4:
                     printf("-------------------------------------\n");
-                    printf("               휴게실       계단\n");
-                    printf("                                    화장실\n");
-                    printf("호실						                       호실\n");
-                    printf("         교무실\n");  
-                    printf("	                                 샤워실\n");
-                    printf("        	    샤워실\n");
-                    printf("\n");
-                    printf("        	             호실\n");
+                    dorm_4();
                     printf("-------------------------------------\n");
                     break;
                 case 5:
                     printf("-------------------------------------\n");
-                    printf("                       계단\n");
-                    printf("\n");
-                    printf("               호실		         호실\n");
-                    printf("\n");  
-                    printf("	                   호실\n");
-                    printf("        	    화장실\n");
+                    dorm_5();
+                    printf("-------------------------------------\n");
                     break;
                 default:
                     printf("잘못된 입력입니다\n");
