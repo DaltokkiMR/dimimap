854b8262159da40421b1cd559f2e6e197f78bffa
kinggot1004-commits <kinggot1004@dimigo.hs.kr>
2026-06-23 08:11:22 +0900

    Update others.c

--- showmap/others/others.c
+++ showmap/others/others.c
@@ -10,4 +10,87 @@ int show_gym_0(){
     printf("                            의자\n");
     return 0;
 }
-
+int sin_guan_1(){
+    printf("                시청각실\n");
+    printf("\n");
+    printf("                       IT\n");
+    printf("                    프로젝트실\n");
+    printf("\n");
+    printf("   사물인터넷서비스\n");
+    printf("      기획실습실      화장실\n");
+    printf("\n");
+    printf("       계단        엘리베이터       휴머노이드연구소     매점\n");
+    printf("                                                                매점 테라스\n");
+    printf("     세미나실                        3학년부 교무실\n");
+    printf("                 입구\n");
+}
+int sin_guan_2(){
+    printf("                시청각실\n");
+    printf("It정보과학          3-6반\n");
+    printf("  영재학급          3-4반\n");
+    printf("방송콘텐츠제작실습실\n");
+    printf(" 멀티미디어실        3-2반\n");
+    printf("윈도우앱창작터실습실\n");
+    printf("      화장실         화장실\n");
+    printf("\n");
+    printf("       계단        엘리베이터              3-5반\n");
+    printf("                                                            음악실\n"); 
+    printf("     연구실                3-1반  3-3반              계단\n");
+    printf("\n");
+}
+int sin_guan_3(){
+    printf("도서관\n");
+    printf("\n");
+    printf("       계단             엘리베이터    데이터베이스프로그래밍실습실\n");
+    printf("                                                            음악실\n"); 
+    printf("                        이사장실   애니메이션컨텐츠  첨단영상            계단\n");
+    printf("                                      제작실습실      제작실\n");
+}
+int dorm_1(){
+    printf("	     신발장      계단          입구\n");
+    printf("독서실\n");
+    printf("						                    화장실\n");
+    printf("\n");
+    printf("	                                       심야자습실\n");
+    printf("        	             신발장\n");
+    printf("\n");
+    printf("        	                       심자실\n");
+}
+int dorm_2(){
+    printf("  입구                계단\n");
+    printf("                                  화장실\n");
+    printf("  교무실                                      호실\n");
+    printf("                                  샤워실\n");
+    printf("\n");  
+    printf("         샤워실\n");
+    printf("\n");
+    printf("        	            호실\n");
+}
+int dorm_3(){
+    printf("               휴게실       계단\n");
+    printf("                                    화장실\n");
+    printf("호실						                       호실\n");
+    printf("         교무실\n");  
+    printf("	                                 샤워실\n");
+    printf("        	    샤워실\n");
+    printf("\n");
+    printf("        	             호실\n");
+}
+int dorm_4(){
+    printf("               휴게실       계단\n");
+    printf("                                    화장실\n");
+    printf("호실						                       호실\n");
+    printf("         교무실\n");  
+    printf("	                                 샤워실\n");
+    printf("        	    샤워실\n");
+    printf("\n");
+    printf("        	             호실\n");
+}
+int dorm_5(){
+    printf("                       계단\n");
+    printf("\n");
+    printf("               호실		         호실\n");
+    printf("\n");  
+    printf("	                   호실\n");
+    printf("        	    화장실\n");
+}
